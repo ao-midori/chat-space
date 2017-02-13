f94007fcede9cd24497c6ff91626ea54bc6d8b9b
ao-midori <a-mitsuki@hotmail.co.jp>
2017-02-13 16:15:35 +0900

    Redirect to sign_in page at clicking sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
+
 end
