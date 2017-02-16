d0a5d1eb62cbd800947cb4e35e2a4d908aa431f3
ao-midori <a-mitsuki@hotmail.co.jp>
2017-02-16 18:05:30 +0900

    Enabled to create a group, edited view files describing with form_for

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,9 +2,16 @@ class GroupsController < ApplicationController
 before_action :authenticate_user!
 
   def new
+    @group = Group.new
   end
 
-  def edit
+  def create
+    Group.create(group_params)
+    redirect_to root_path
   end
 
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
