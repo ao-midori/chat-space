59a75aa84ad603d4961412e8daef0aeaf9edb664
ao-midori <a-mitsuki@hotmail.co.jp>
2017-02-20 12:50:46 +0900

    Created Message model & table, and add associations with it

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ApplicationRecord
   validates :name, presence: true
 
-  has_many :group_users
-  has_many :users, through: :group_users
   accepts_nested_attributes_for :users
+  has_many :users, through: :group_users
+  has_many :group_users
+  has_many :messages
 end
