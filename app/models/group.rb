94f6c0087f9c0be2e5b7f1e089a74d8b520b1156
ao-midori <a-mitsuki@hotmail.co.jp>
2017-02-22 15:03:35 +0900

    Edited model files to make looks neat

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,9 @@
 class Group < ApplicationRecord
   validates :name, presence: true
 
-
-  has_many :users, through: :group_users
   has_many :group_users
-  accepts_nested_attributes_for :users
+  has_many :users, through: :group_users
   has_many :messages
+
+  accepts_nested_attributes_for :users
 end
