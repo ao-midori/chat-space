044c904e947bf97f7fa1408ff732108c1b7144dd
ao-midori <a-mitsuki@hotmail.co.jp>
2017-03-02 20:46:47 +0900

    Edited example description

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe Message do
   describe '#create' do
 
-    it "is valid with a message" do
+    it "is valid with a body" do
       message = build(:message)
       message.valid?
       expect(message).to be_valid
     end
 
-    it "is invalid without a message" do
+    it "is invalid without a body" do
       message = build(:message, body: "")
       message.valid?
       expect(message).not_to be_valid
