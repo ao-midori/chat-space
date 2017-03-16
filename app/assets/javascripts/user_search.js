c78b4cf03b33fac078f0cbdaa1d38f4dddc2cf5c
ao-midori <a-mitsuki@hotmail.co.jp>
2017-03-16 17:02:56 +0900

    Fixed wrong behavior of adding members

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,28 +1,30 @@
 $(function() {
-  var preWord = "";
+  var preInput = "";
 
   function appendList(user) {
-    var item = `<li class="list" id="${user.id}">${user.name}</li>`;
+    var item = `<li class="list result-list" user_id="${user.id}" user_name="${user.name}">${user.name}</li>`;
     $('.user-search-result').append(item);
   }
 
   function appendAddBtn() {
     var add_btn = '<span class="chat-group-user__btn--add">追加</span>'
-    $('.list').append(add_btn);
+    $('.result-list').append(add_btn);
   }
 
-  $('#chat-group-form__user').on('keyup', function() {
-    var input = $(this).val();
+  $(document).on('turbolinks:load', function() {
+    $('#chat-group-form__user').on('keyup', function() {
+      var input = $(this).val();
 
-    if (input !== preWord && input.length !== 0) {
+    if (input !== preInput && input.length !== 0) {
       $.ajax({
         type: 'GET',
         url: '/groups/user_search.json',
         data: { content : input },
         dataType: 'json'
       })
+
       .done(function(data) {
-        $('.list').remove();
+        $('.result-list').remove();
         $.each(data, function(i, user) {
           appendList(user);
         });
@@ -34,5 +36,14 @@ $(function() {
       });
     }
   });
+
+  $(document).on('click', '.chat-group-user__btn--add', function() {
+    var add_id = $(this).parent().attr('user_id');
+    var add_name = $(this).parent().attr('user_name');
+    var add_item = `<li class="list" user_id="${add_id}" user_name="${add_name}">${add_name}<span class="chat-group-user__btn--remove">削除</span></li>`;
+    $(this).parent().remove();
+    $('.chat-group-user').append(add_item);
+  });
+});
 });
 
