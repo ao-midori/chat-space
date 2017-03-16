7ad677d1f787a8919f9e889e16291664dae509c7
ao-midori <a-mitsuki@hotmail.co.jp>
2017-03-16 17:34:48 +0900

    Added member removing behavior

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,49 +1,52 @@
 $(function() {
   var preInput = "";
 
-  function appendList(user) {
-    var item = `<li class="list result-list" user_id="${user.id}" user_name="${user.name}">${user.name}</li>`;
+  function appendList(id, name) {
+    var item = `<li class="list result-list" user_id="${id}" user_name="${name}">${name}<span class="chat-group-user__btn--add">追加</span></li>`;
     $('.user-search-result').append(item);
   }
 
-  function appendAddBtn() {
-    var add_btn = '<span class="chat-group-user__btn--add">追加</span>'
-    $('.result-list').append(add_btn);
-  }
-
   $(document).on('turbolinks:load', function() {
+
     $('#chat-group-form__user').on('keyup', function() {
       var input = $(this).val();
-
-    if (input !== preInput && input.length !== 0) {
-      $.ajax({
-        type: 'GET',
-        url: '/groups/user_search.json',
-        data: { content : input },
-        dataType: 'json'
-      })
-
-      .done(function(data) {
-        $('.result-list').remove();
-        $.each(data, function(i, user) {
-          appendList(user);
+      if (input !== preInput && input.length !== 0) {
+        $.ajax({
+          type: 'GET',
+          url: '/groups/user_search.json',
+          data: { content : input },
+          dataType: 'json'
+        })
+        .done(function(data) {
+          $('.result-list').remove();
+          $.each(data, function(i, user) {
+            appendList(user.id, user.name);
+          });
+        })
+        .fail(function() {
+        alert('error');
         });
-        appendAddBtn();
-      })
+      }
+    });
 
-      .fail(function() {
-      alert('error');
-      });
-    }
-  });
+    $(document).on('click', '.chat-group-user__btn--add', function() {
+      var target = $(this).parent();
+      var add_id = $(target).attr('user_id');
+      var add_name = $(target).attr('user_name');
+      var add_item = `<li class="list select_member" user_id="${add_id}" user_name="${add_name}">${add_name}<span class="chat-group-user__btn--remove">削除</span></li>`;
 
-  $(document).on('click', '.chat-group-user__btn--add', function() {
-    var add_id = $(this).parent().attr('user_id');
-    var add_name = $(this).parent().attr('user_name');
-    var add_item = `<li class="list" user_id="${add_id}" user_name="${add_name}">${add_name}<span class="chat-group-user__btn--remove">削除</span></li>`;
-    $(this).parent().remove();
-    $('.chat-group-user').append(add_item);
+      $(target).remove();
+      $('.chat-group-user').append(add_item);
+    });
+
+    $(document).on('click', '.chat-group-user__btn--remove', function() {
+      var target = $(this).parent();
+      var remove_id = $(target).attr('user_id');
+      var remove_name = $(target).attr('user_name');
+
+      appendList(remove_id, remove_name);
+      $(target).remove();
+    });
   });
-});
 });
 
