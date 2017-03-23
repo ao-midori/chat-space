8dd6871c43594de6d8a8e32fc5b571d46b754726
ao-midori <a-mitsuki@hotmail.co.jp>
2017-03-23 15:11:22 +0900

    Fixed setInterval & clearInterval timing

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -35,39 +35,39 @@ $(function() {
     });
 
     // 最新メッセージの自動更新
-      // チャット画面において、3秒毎に更新有無チェック
     var url = document.location.pathname;
+    var messagePolling;
+
+      // チャット画面表示時、3秒毎に更新有無チェック
     if (url.match(/messages/)) {
-      if (window.set_timer_on == null) {
-        window.timer = setInterval(autoUpdate, 3000);
-      }
-    } else {
-      clearInterval(window.timer)
-      window.timer = null
+      messagePolling = setInterval(autoUpdate, 3000);
+      $(document).on('turbolinks:load', clearPolling);
+    }
+
+    function clearPolling() {
+      clearInterval(messagePolling);
     }
 
       // 最新メッセージ有の場合、追加表示
     function autoUpdate() {
       var message_number = $('.message-box').length;
-      if (url.match(/messages/)) {
-        $.ajax({
-          type:'GET',
-          url: url,
-          dataType: 'json'
-        })
-        .done(function(data) {
-          if ( message_number !== data.messages.length ) {
-            for ( var i = message_number; i < data.messages.length; i++ ) {
-              var html = buildHTML(data.messages[i]);
-              $('#message-wrapper').append(html);
-              $('#message-wrapper').animate({scrollTop: $('#message-wrapper')[0].scrollHeight}, 1000);
-            }
+      $.ajax({
+        type:'GET',
+        url: url,
+        dataType: 'json'
+      })
+      .done(function(data) {
+        if ( message_number !== data.messages.length ) {
+          for ( var i = message_number; i < data.messages.length; i++ ) {
+            var html = buildHTML(data.messages[i]);
+            $('#message-wrapper').append(html);
+            $('#message-wrapper').animate({scrollTop: $('#message-wrapper')[0].scrollHeight}, 1000);
           }
-        })
-        .fail(function() {
-          alert('error');
-        });
-      }
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
     }
   });
 });
